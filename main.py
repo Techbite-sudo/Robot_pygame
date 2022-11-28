12f9d97bb805ec3a87f327443c9d79915674d2c9
Techbite <bonfacemwema7@gmail.com>
2022-11-28 13:24:15 +0300

    code restructure and addition of .wav files

--- main.py
+++ main.py
@@ -1,38 +1,39 @@
 import pygame
 import sys
+from pygame import mixer
 
 # General setup
 pygame.init()
 clock = pygame.time.Clock()
 
-class Button:
-    def __init__(self, text, pos, fontSize, bg="black", feedback=""):
-        self.x, self.y = pos
-        self.font = pygame.font.SysFont("Arial", fontSize)
-        if feedback == "":
-            self.feedback = "text"
-        else:
-            self.feedback = feedback
-        self.change_text(text, bg)
 
-    def change_text(self, text, bg="black"):
-        self.text = self.font.render(text, 1, pygame.Color("White"))
-        self.size = self.text.get_size()
-        self.surface = pygame.Surface(self.size)
-        self.surface.fill(bg)
-        self.surface.blit(self.text, (0, 0))
-        self.rect = pygame.Rect(self.x, self.y, self.size[0], self.size[1])
+# button class
+class Button():
+    def __init__(self, x, y, image, scale):
+        widthbtn = image.get_width()
+        heightbtn = image.get_height()
+        self.image = pygame.transform.scale(image, (int(widthbtn * scale), int(heightbtn * scale)))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+        self.clicked = False
 
-    def show(self):
-        screen.blit(button1.surface, (self.x, self.y))
+    def draw(self, surface):
+        action = False
+        # get mouse position
+        pos = pygame.mouse.get_pos()
+        # check mouseover and clicked conditions
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                action = True
 
-    def click(self, event):
-        x, y = pygame.mouse.get_pos()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if pygame.mouse.get_pressed()[0]:
-                if self.rect.collidepoint(x, y):
-                    player.animate()
-                    self.change_text(self.feedback, bg="red")
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+
+        # draw button on screen
+        surface.blit(self.image, (self.rect.x, self.rect.y))
+
+        return action
 
 
 def zoom(path, sizeX):
@@ -45,40 +46,40 @@ class Player(pygame.sprite.Sprite):
         self.sequence_animation = False
         self.sprites = []
 
-        self.sprites.append(zoom("./new/Subject 6.png", 150))
-        self.sprites.append(zoom("./new/Subject 7.png", 150))
-        self.sprites.append(zoom("./new/Subject 8.png", 173))
-        self.sprites.append(zoom("./new/Subject 9.png", 189))
-        self.sprites.append(zoom("./new/Subject 10.png", 203))
-        self.sprites.append(zoom("./new/Subject 11.png", 192))
-        self.sprites.append(zoom("./new/Subject 12.png", 164))
-        self.sprites.append(zoom("./new/Subject 12.png", 164))
-        self.sprites.append(zoom("./new/Subject 14.png", 164))
-        self.sprites.append(zoom("./new/Subject 14.png", 164))
-        self.sprites.append(zoom("./new/Subject 14.png", 164))
-        self.sprites.append(zoom("./new/Subject 12.png", 164))
-        self.sprites.append(zoom("./new/Subject 12.png", 164))
-        self.sprites.append(zoom("./new/Subject 16.png", 164))
-        self.sprites.append(zoom("./new/Subject 16.png", 164))
-        self.sprites.append(zoom("./new/Subject 16.png", 164))
-        self.sprites.append(zoom("./new/Subject 12.png", 164))
-        self.sprites.append(zoom("./new/Subject 12.png", 164))
-        self.sprites.append(zoom("./new/Subject 11.png", 192))
-        self.sprites.append(zoom("./new/Subject 10.png", 203))
-        self.sprites.append(zoom("./new/Subject 9.png", 189))
-        self.sprites.append(zoom("./new/Subject 8.png", 173))
-        self.sprites.append(zoom("./new/Subject 7.png", 150))
-        self.sprites.append(zoom("./new/Subject 6.png", 150))
-        self.sprites.append(zoom("./new/Subject 17.png", 150))
-        self.sprites.append(zoom("./new/Subject 17.png", 150))
-        self.sprites.append(zoom("./new/Subject 18.png", 150))
-        self.sprites.append(zoom("./new/Subject 18.png", 150))
-        self.sprites.append(zoom("./new/Subject 19.png", 150))
-        self.sprites.append(zoom("./new/Subject 19.png", 150))
-        self.sprites.append(zoom("./new/Subject 18.png", 150))
-        self.sprites.append(zoom("./new/Subject 18.png", 150))
-        self.sprites.append(zoom("./new/Subject 17.png", 150))
-        self.sprites.append(zoom("./new/Subject 17.png", 150))
+        self.sprites.append(zoom("images/Subject 6.png", 150))
+        self.sprites.append(zoom("images/Subject 7.png", 150))
+        self.sprites.append(zoom("images/Subject 8.png", 173))
+        self.sprites.append(zoom("images/Subject 9.png", 189))
+        self.sprites.append(zoom("images/Subject 10.png", 203))
+        self.sprites.append(zoom("images/Subject 11.png", 192))
+        self.sprites.append(zoom("images/Subject 12.png", 164))
+        self.sprites.append(zoom("images/Subject 12.png", 164))
+        self.sprites.append(zoom("images/Subject 14.png", 164))
+        self.sprites.append(zoom("images/Subject 14.png", 164))
+        self.sprites.append(zoom("images/Subject 14.png", 164))
+        self.sprites.append(zoom("images/Subject 12.png", 164))
+        self.sprites.append(zoom("images/Subject 12.png", 164))
+        self.sprites.append(zoom("images/Subject 16.png", 164))
+        self.sprites.append(zoom("images/Subject 16.png", 164))
+        self.sprites.append(zoom("images/Subject 16.png", 164))
+        self.sprites.append(zoom("images/Subject 12.png", 164))
+        self.sprites.append(zoom("images/Subject 12.png", 164))
+        self.sprites.append(zoom("images/Subject 11.png", 192))
+        self.sprites.append(zoom("images/Subject 10.png", 203))
+        self.sprites.append(zoom("images/Subject 9.png", 189))
+        self.sprites.append(zoom("images/Subject 8.png", 173))
+        self.sprites.append(zoom("images/Subject 7.png", 150))
+        self.sprites.append(zoom("images/Subject 6.png", 150))
+        self.sprites.append(zoom("images/Subject 17.png", 150))
+        self.sprites.append(zoom("images/Subject 17.png", 150))
+        self.sprites.append(zoom("images/Subject 18.png", 150))
+        self.sprites.append(zoom("images/Subject 18.png", 150))
+        self.sprites.append(zoom("images/Subject 19.png", 150))
+        self.sprites.append(zoom("images/Subject 19.png", 150))
+        self.sprites.append(zoom("images/Subject 18.png", 150))
+        self.sprites.append(zoom("images/Subject 18.png", 150))
+        self.sprites.append(zoom("images/Subject 17.png", 150))
+        self.sprites.append(zoom("images/Subject 17.png", 150))
 
         self.current_sprite = 0
         self.image = self.sprites[self.current_sprite]
@@ -95,54 +96,53 @@ class Player(pygame.sprite.Sprite):
             if int(self.current_sprite) >= len(self.sprites):
                 self.current_sprite = 0
                 self.sequence_animation = False
-                button1.change_text("Animate", bg="navy")
+                # button1.change_text("Animate", bg="navy")
 
         self.image = self.sprites[int(self.current_sprite)]
+
+
 # Game Screen
 screen_width = 848
 screen_height = 477
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Sprite Animation")
+pygame.display.set_caption("Animate")
 
-# Creating the sprites and groups
-moving_sprites = pygame.sprite.Group()
+# load button images and background image
+animate_img = pygame.image.load('images/animate.png').convert_alpha()
+bg = pygame.image.load("images/bg.webp").convert_alpha()
+
+# create button instances, player instances and groups
 player = Player(300, 250)
+animate_button = Button(350, 5, animate_img, 0.5)
+moving_sprites = pygame.sprite.Group()
 moving_sprites.add(player)
-bg = pygame.image.load("./new/bg.webp")
-
-width = screen.get_width()
-height = screen.get_height()
-color_white = (255, 255, 255)
-color_light = (170, 170, 170)
-color_dark = (100, 100, 100)
 
 fps = 0.15
 
-button1 = Button(
-    "Animate",
-    (340, 430),
-    fontSize=30,
-    bg="navy",
-    feedback="Animating"
-)
+# Background sound
+mixer.music.load('audio/Robot-Songs-Robot-says.wav')
+mixer.music.play(-1)
 
-while True:
+# game loop
+run = True
+while run:
+    # screen.fill((0, 0, 0))
+
+    if animate_button.draw(screen):
+        player.animate()
+    # event handler
     for event in pygame.event.get():
+        # quit game
         if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.KEYDOWN:
-            player.animate()
+            run = False
 
     # Drawing
-    mouse = pygame.mouse.get_pos()
     screen.fill([255, 255, 255])
     screen.blit(pygame.transform.smoothscale(bg, (848, 477)), (0, 0))
 
-    button1.click(event)
-    button1.show()
-
     moving_sprites.draw(screen)
+    # animate_button.draw(screen)
+    pygame.display.update()
     moving_sprites.update(fps)
     pygame.display.flip()
     clock.tick(60)
